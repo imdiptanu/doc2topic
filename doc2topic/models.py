47e2c9bf52a4c61d32a7204969163a73df617668
Diptanu Sarkar <diptanusarkar@hotmail.com>
2021-05-04 15:55:03 -0400

    Debug

--- doc2topic/models.py
+++ doc2topic/models.py
@@ -123,6 +123,11 @@ class Doc2Topic:
 
 	def get_topic_words(self, top_n=10, stopwords=set()):
 		self.get_wordvecs()
+		print("-----------------")
+		print(self.wordvecs)
+		self.get_docvecs()
+		print(self.docvecs)
+		print("-----------------")
 		topic_words = {}
 		for topic in range(self.wordvecs.shape[1]):
 			topic_words[topic] = heapq.nlargest(top_n+len(stopwords), enumerate(L1normalize(self.wordvecs[:,topic])), key=lambda x:x[1])
